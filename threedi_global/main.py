6ffcf6a4b84a450566026862a26e66f31bd8efe1
leendertvanwolfswinkel <leendert.vanwolfswinkel@nelen-schuurmans.nl>
2022-09-08 15:30:25 +0200

    [WIP] Make fully configurable and easy to handle

--- threedi_global/main.py
+++ threedi_global/main.py
@@ -17,6 +17,7 @@ Step 3: Make tables
 Step 4: Create and upload sqlite
     
 """
+from math import sqrt
 from typing import Union, Tuple
 import time
 from pathlib import Path
@@ -29,7 +30,6 @@ from threedi_api.upload import upload_and_process
 from login import get_login_details
 from epsg import utm_zone_epsg_for_polygon
 
-DEM_UUID = "eae92c48-cd68-4820-9d82-f86f763b4186"
 HEADERS = {
     "username": "__key__",
     "password": get_login_details(section="lizard", option="api_key"),
@@ -37,25 +37,30 @@ HEADERS = {
 }
 SQL_DIR = Path(__file__).parent / "sql"
 DATA_DIR = Path(__file__).parent / "data"
-SPATIALITE_TEMPLATE_PATH = DATA_DIR / "leeg.sqlite"
+SPATIALITE_TEMPLATE_PATH = DATA_DIR / "template.sqlite"
+NR_CELLS = 50000
 
 
-def read_geom(filename: Union[Path, str]) -> Tuple[str, int, int]:
+def read_geom(filename: Union[Path, str]) -> Tuple[str, int, int, float]:
     """
     Returns:
         - the geometry in WKT format of the first feature of the first layer of an OGR compatible vector dataset
         - the EPSG code of the layer that was read
         - the EPSG code of the UTM Zone the centroid of the geometry is in
+        - area (in units of the input polygon's CRS)
     """
     datasource = ogr.Open(str(filename))
     layer = datasource.GetLayer(0)
     srs = layer.GetSpatialRef()
+    if not srs.IsProjected():
+        raise ValueError("Input polygon does not have a Projected CRS. Cannot calculate its area in meters.")
     epsg_code = srs.GetAuthorityCode(None)
     feature = layer.GetFeature(0)
     geom = feature.GetGeometryRef()
     utm_zone_epsg = utm_zone_epsg_for_polygon(geom=geom, srs=srs)
     geom_wkt = geom.ExportToWkt()
-    return geom_wkt, epsg_code, utm_zone_epsg
+    geom_area = geom.GetArea()
+    return geom_wkt, epsg_code, utm_zone_epsg, geom_area
 
 
 def download_rasters(
@@ -120,55 +125,41 @@ def download_file(url, output_path: Union[Path, str], headers):
     return
 
 
-def fill_settings(sqlite_filename, epsg_code):
+def optimal_grid_space(model_area: float, pixel_size: float, nr_cells: int = 50000):
+    # model_area = 1000000
+    # pixel_size = 0.25
+    # nr_cells = 50.000
+    # should return 4.5
+    raw_answer = sqrt(model_area/nr_cells)
+    double_pixel_size = 2*pixel_size
+    multiple_of_double_pixel_size = max(double_pixel_size, double_pixel_size * round(raw_answer/double_pixel_size))
+    return multiple_of_double_pixel_size
+
+
+def fill_settings(sqlite_filename, epsg_code, grid_space):
     """
     This functions takes an empty 3Di spatialite and updates the numerical and global settings
-    #TODO: Check of er nog andere instellingen gedaan moeten worden.
     """
     # Path to spatialite
-    sql_fn = str(SQL_DIR / 'fill_2d_settings.sql')
-    with open(sql_fn, 'r') as sql_file:
-        query = sql_file.read()
-        formatted_query = query.format(epsg_code=epsg_code)
     conn = sqlite3.connect(sqlite_filename)
     c = conn.cursor()
-    c.executescript(formatted_query)
+    sql = """UPDATE v2_global_settings SET epsg_code = {epsg_code}, grid_space = {grid_space};"""
+    c.execute(sql)
     return
 
 
-def create_threedimodel(
-        local_dir: Union[Path, str],
-        extent_filename: Union[Path, str],
-        name_model: str,
-        pixel_size
-):
-    """
-    This function is the main framework to create a 2D 3Di model
-    This is done by excecuting the following steps
-    
-    1. Create folder structure
-        folder 
-        rasters
-    
-    2. Download files
-    3. Create and update sqlite
-    4. Upload to 3Di via schematisation etc
-
-    """
-
-    # 1. Create folder structue
-    print("Creating local (temp) folder structure...")
+def create_temp_dir(local_dir: [Path, str], schematisation_name: str):
     local_dir = Path(local_dir)
-    extent_filename = Path(extent_filename)
-    schematisation_dir = local_dir / name_model
+    schematisation_dir = local_dir / schematisation_name
     rasters_dir = schematisation_dir / "rasters"
     rasters_dir.mkdir(parents=True, exist_ok=True)
 
-    # 2. Download raster (Only DEM in this case)
-    print("Requesting DEM download link from Lizard...")
-    geom_extent, extent_epsg_code, utm_zone_epsg = read_geom(extent_filename)
+
+def download_dem(local_dir: [Path, str], schematisation_name: str, uuid, extent_filename: [Path, str], pixel_size):
+    extent_filename = Path(extent_filename)
+    geom_extent, extent_epsg_code, utm_zone_epsg, area = read_geom(extent_filename)
     dem_download_url = download_rasters(
-        DEM_UUID,
+        uuid,
         pixel_size=pixel_size,
         extent_wkt=geom_extent,
         extent_srs=f"EPSG:{extent_epsg_code}",
@@ -176,24 +167,55 @@ def create_threedimodel(
         headers=HEADERS
     )
     print("Downloading DEM...")
-    dem_path = rasters_dir / "dem.tif"
+    dem_path = Path(local_dir) / schematisation_name / "rasters" / "dem.tif"
     download_file(url=dem_download_url, output_path=dem_path, headers=HEADERS)
 
-    # 3. Create and update sqlite (3Di model) plus zip the file
-    print("Creating Sqlite...")
-    spatialite_dst_path = schematisation_dir / f"{name_model}.sqlite"
-    shutil.copyfile(SPATIALITE_TEMPLATE_PATH, spatialite_dst_path)
-    fill_settings(spatialite_dst_path, utm_zone_epsg)
 
-    # 4. Make revision and schematisation
-    print("Create schematisation revision...")
-    raster_names = {"dem_file": "dem"}
-    tags = ["W0176", "Global", "demomodel"]
-    schematisation_name = name_model
+def prepare_spatialite(
+        local_dir: [Path, str],
+        schematisation_name: str,
+        extent_filename: [Path, str],
+        pixel_size: float,
+        nr_cells
+):
+    spatialite_dst_path = Path(local_dir) / schematisation_name / f"{schematisation_name}.sqlite"
+    shutil.copyfile(SPATIALITE_TEMPLATE_PATH, spatialite_dst_path)
+    geom_extent, extent_epsg_code, utm_zone_epsg, area = read_geom(extent_filename)
+    grid_space = optimal_grid_space(model_area=area, pixel_size=pixel_size, nr_cells=nr_cells)
+    fill_settings(spatialite_dst_path, epsg_code=utm_zone_epsg, grid_space=grid_space)
+
+
+def create_threedimodel(
+        local_dir: Union[Path, str],
+        schematisation_name: str,
+        extent_filename: Union[Path, str],
+        pixel_size,
+        nr_cells,
+        dem_raster_uuid: str = "eae92c48-cd68-4820-9d82-f86f763b4186"
+):
+    print("Creating local (temp) folder structure...")
+    create_temp_dir(local_dir=local_dir, schematisation_name=schematisation_name)
+    print("Requesting DEM download link from Lizard...")
+    download_dem(
+        local_dir=local_dir,
+        schematisation_name=schematisation_name,
+        extent_filename=extent_filename,
+        pixel_size=pixel_size,
+        uuid=dem_raster_uuid
+    )
+    print("Preparing sqlite...")
+    prepare_spatialite(
+        local_dir=local_dir,
+        schematisation_name=schematisation_name,
+        extent_filename=extent_filename,
+        pixel_size=pixel_size,
+        nr_cells=nr_cells
+    )
+    print("Uploading...")
     threedimodel_id, schematisation_id = upload_and_process(
         schematisation_name=schematisation_name,
-        sqlite_path=spatialite_dst_path,
-        raster_names=raster_names,
-        schematisation_create_tags=tags
+        sqlite_path=Path(local_dir) / schematisation_name / f"{schematisation_name}.sqlite",
+        raster_names={"dem_file": "dem"}
     )
     print(f"Created 3Di Model with ID {threedimodel_id}: https://management.3di.live/schematisations/{schematisation_id}")
+
--- threedi_global/test.py
+++ threedi_global/test.py
@@ -1,8 +1,11 @@
 from main import create_threedimodel
-
 create_threedimodel(
     local_dir="C:/Temp/3Di Global",
+    schematisation_name="Chamba",
     extent_filename="C:/Temp/3Di Global/test_extent_india.shp",
-    name_model="Chamba",
-    pixel_size=10
+    pixel_size=10,
+    nr_cells=50000,
+    dem_raster_uuid="eae92c48-cd68-4820-9d82-f86f763b4186"
 )
+
+
